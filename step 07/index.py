078e4c20413343580010bc24f0e645637847587d
Muhammad Rafi Ali <therafiali@gmail.com>
2023-10-27 06:57:24 +0500

    step 07

---
+++ step 07/index.py
@@ -0,0 +1,10 @@
+import sys
+
+print("line1")
+print("line2")
+print(sys.argv) # iterative data type list[str] 0= filename **value use define
+
+# enter in terminal : D:\Learn Python\step 07>python index.py 123
+#result = line1
+# line2
+# ['index.py', '123']
